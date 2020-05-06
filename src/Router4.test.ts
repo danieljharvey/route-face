1c931b0691d69f822dc6c2db6745fe830f3d2e12
Daniel Harvey <danieljamesharvey@gmail.com>
2020-05-06 19:08:01 +0100

    Oh yeah

--- src/Router4.test.ts
+++ src/Router4.test.ts
@@ -1,25 +1,16 @@
 import * as R from './Router4'
 import * as t from 'io-ts'
+import * as Res from './Result'
 import { getArbitrary } from 'fast-check-io-ts'
 import fc from 'fast-check'
 
-const myRoute = R.add(
-  R.add(
-    R.add(R.pure(t.literal('bum')), t.literal('town')),
-    t.number
-  ),
-  t.string
-)
-
-const myRouteArb = getArbitrary(t.tuple(myRoute.pieces))
-
-const myRoute2 = R.makeRoute
+const myRoute = R.makeRoute
   .then(t.literal('dog'))
   .then(t.literal('log'))
   .then(t.literal('bog'))
   .done()
 
-const myRouteArb2 = getArbitrary(t.tuple(myRoute2.pieces))
+const myRouteArb = getArbitrary(t.tuple(myRoute.pieces))
 
 describe('Router4', () => {
   it('Creates arbitrary valid endpoints', () => {
@@ -27,11 +18,7 @@ describe('Router4', () => {
       fc.property(myRouteArb, (arb) => {
         const stringPieces = arb.map(String)
         const validated = R.validate(myRoute, stringPieces)
-        console.log(validated)
-        return (
-          validated.type === 'Failure' ||
-          validated.type === 'Success'
-        )
+        return Res.isSuccess(validated)
       })
     )
   })
